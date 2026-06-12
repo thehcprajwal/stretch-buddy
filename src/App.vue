c7bf527b3c5228cf8f21f7f3388743088f2b8cc0
H C Prajwal <hcprajwal.dev@gmail.com>
2026-06-12 09:22:00 +0530

    add App.vue frameless popover shell

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app class="popover-app">
+    <v-main>
+      <StretchReminder />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import StretchReminder from './components/StretchReminder.vue'
+</script>
+
+<style>
+/* Make the app flush with the frameless window edges */
+html, body, #app {
+  width: 380px;
+  height: 480px;
+  overflow: hidden;
+  background: transparent;
+}
+
+.popover-app {
+  width: 380px !important;
+  min-height: unset !important;
+}
+</style>
